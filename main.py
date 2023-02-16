56cb10384f699b19ab55932e8b1f863ebca89585
masterbbud <45081958+masterbbud@users.noreply.github.com>
2023-02-16 08:09:51 -0500

    Added new backup

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import date, datetime
 
 text = requests.get('https://www.se.rit.edu/~swen-261/00/schedule-spring-2perTR-NEW-project.html').text
 
-today = datetime.today().strftime('%m-%d-%Y')
 
-with open(f'logs/{today}-oss.html', 'w') as f:
+with open(f'logs/current-oss.html', 'w') as f:
     f.write(text)
